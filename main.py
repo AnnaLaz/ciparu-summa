ecf2b8161a543aee7ab0dddc580028e60a347d06
Анюта Лазарева <78086901+AnnaLaz@users.noreply.github.com>
2022-02-01 14:04:21 +0000

    k.griboka, k.jefimova, a.lazareva

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-teksts = input("Ievadi tekstu:")
-def replaceTwos(teksts):
-  if teksts.count("2")>0:
-    teksts = teksts.replace("2","divi")
-    print (teksts)
-  else:
-    teksts= "Nekas netika aizvietots"
-    print (teksts)
-  return teksts
-replaceTwos(teksts)
+teksts=input("Ievadi vārdu:  ")
+skaititais=0
+for burts in teksts:
+  if burts=='a':
+    skaititais+=1
+print("Burtu a skaits:", skaititais)
+
+
+# teksts = input("Ievadi vārdu:  ")
+# def countZeros(teksts):
+#   return teksts.count("a")
+# print ("Burtu a skaits:",countZeros(teksts))
+
+
+# teksts = input("Ievadi tekstu:")
+# def replaceTwos(teksts):
+#   if teksts.count("4")>0:
+#     teksts = teksts.replace("4","četri")
+#     print (teksts)
+#   else:
+#     teksts= "Nekas netika aizvietots"
+#     print (teksts)
+#   return teksts
+# replaceTwos(teksts) 
